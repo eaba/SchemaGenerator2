c9825dae11ab47538e75211f4b8f0daf688c9cfb
Ebere Abanonu <eabanonu@yahoo.com>
2022-01-16 15:52:14 +0100

    Add project files.

---
+++ SchemaGenerator/DefaultValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+
+namespace AvroSchemaGenerator
+{
+    public static class DefaultValue
+    {
+        public static (bool required, bool hasDefault, object defaultValue) GetSchemaCustomAttributes(this PropertyInfo property)
+        {
+            var required = IsRequiredProperty(property);
+            var dv = GetDefaultValueForProperty(property);
+            return (required, dv.hasDefault, dv.defaultValue);
+        }
+        private static (bool hasDefault, object defaultValue) GetDefaultValueForProperty(PropertyInfo property)
+        {
+            var defaultAttr = (DefaultValueAttribute)property.GetCustomAttribute(typeof(DefaultValueAttribute));
+            return defaultAttr != null ? (true, defaultAttr.Value) : (false, null);
+        }
+        private static bool IsRequiredProperty(PropertyInfo property)
+        {
+            return property.GetCustomAttribute(typeof(RequiredAttribute)) != null;
+        }
+        
+    }
+}
